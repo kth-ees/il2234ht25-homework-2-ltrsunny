6909e59a69bc54b71c7622de31d3b0d7608fe914
Liu Taoran <ltrsunny@gmail.com>
2025-09-27 00:09:51 +0800

    Q2

--- q2/LFSR_6bit.sv
+++ q2/LFSR_6bit.sv
@@ -4,7 +4,16 @@ module LFSR_6bit (
   input  logic [5:0] parallel_in,
   output logic [5:0] parallel_out
 );
-  // …
-  // Add your description here
-  // …
+      always_ff @( posedge clk or negedge rst_n ) begin 
+        if(!rst_n) parallel_out <= 6'b0;  // Asynchronous reset
+        else if (sel) begin // LFSR operation
+            parallel_out[0] <= parallel_out[5];
+            parallel_out[1] <= parallel_out[0] ^ parallel_out[5];
+            parallel_out[2] <= parallel_out[1];
+            parallel_out[3] <= parallel_out[2] ^ parallel_out[5];    
+            parallel_out[4] <= parallel_out[3];   
+            parallel_out[5] <= parallel_out[4];
+        end
+        else if (!sel) parallel_out <= parallel_in;         // Parallel loading           
+    end
 endmodule
--- q2/LFSR_6bit_tb.sv
+++ q2/LFSR_6bit_tb.sv
@@ -1,5 +1,41 @@
 module LFSR_6bit_tb;
 
-// complete here
+    logic clk;
+    logic rst_n;
+    logic sel;
+    logic [5:0] parallel_in;
+    logic [5:0] parallel_out;
+
+    LFSR_6bit dut (
+        .clk(clk),
+        .rst_n(rst_n),
+        .sel(sel),
+        .parallel_in(parallel_in),
+        .parallel_out(parallel_out)
+    );
+
+    always #5 clk = ~clk;
+
+    initial begin
+
+        clk = 0;
+        rst_n = 0;
+        sel = 0;
+        parallel_in = 6'b011000;  
+        #20;
+        
+        rst_n = 1;
+        #20;
+
+        sel = 1;
+        #50;  
+
+        sel = 0;
+        parallel_in = 6'b001100;
+        #20;
+
+        sel = 1;
+        #50;
+    end
 
 endmodule
